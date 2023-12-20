080d69f24e554683b11ce24e1289e31690ed17de
Hongjun Lee <zunza96@gmail.com>
2023-12-20 17:17:39 +0900

    [CHORE]: README.md 추가, post 도메인 factory method 삭제 -> builder로 변경

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jpa Sample
+
+## 기술스택
+1. Language: Java 17
+2. Framework & Library: Spring Boot 3.2.0, JPA
+3. Database: MySQL
+4. CI/CD: Docker
--- src/main/java/com/example/post/domain/Post.java
+++ src/main/java/com/example/post/domain/Post.java
@@ -12,20 +12,5 @@ public class Post {
     private final String title;
     private final String content;
     private final List<Comment> comments;
-//    public static Post withId(PostId postId, String title, String content, List<Comment> comments){
-//        return Post.builder()
-//                .id(postId)
-//                .title(title)
-//                .content(content)
-//                .comments(comments)
-//                .build();
-//    }
-//    public static Post withoutId(String title, String content){
-//        return Post.builder()
-//                .title(title)
-//                .content(content)
-//                .build();
-//    }
-
     public record PostId(Long value) {}
 }
